1995254f4cab58b5641ac8290b2a67ac8d4e9319
BlackMesaDude <francescomesianodev@gmail.com>
2020-12-28 21:51:40 +0100

    Added IEntity.cs

---
+++ Server/ANLS/Networking/Entity/Interfaces/IEntity.cs
@@ -0,0 +1,13 @@
+namespace ANL.Server.Networking.Entity.Interfaces 
+{
+    /// <summary>
+    /// Defines a entity structure
+    /// </summary>
+    public interface IEntity 
+    {
+        /// <summary>
+        /// Update behaviour for this entity
+        /// </summary>
+        void Update();
+    }
+}
