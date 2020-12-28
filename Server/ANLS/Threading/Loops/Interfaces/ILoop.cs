dc6c29767e1487099985493ec49d2dad1f21e491
BlackMesaDude <francescomesianodev@gmail.com>
2020-12-28 21:52:50 +0100

    Added ILoop.cs

---
+++ Server/ANLS/Threading/Loops/Interfaces/ILoop.cs
@@ -0,0 +1,13 @@
+namespace ANL.Server.Threading.Loops.Interfaces 
+{
+    /// <summary>
+    /// Contains basic looping methods
+    /// </summary>
+    public interface ILoop
+    {
+        /// <summary>
+        /// Defines the update behaviour
+        /// </summary>
+        void Update();
+    }
+}
